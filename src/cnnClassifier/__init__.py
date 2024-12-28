beee407e4303902d89541236e97a8b4d691942dd
unknown <vallee.neutral@gmail.com>
2024-12-28 14:16:20 +0000

    data ingestion complete

--- src/cnnClassifier/__init__.py
+++ src/cnnClassifier/__init__.py
@@ -8,14 +8,13 @@ log_dir = "logs"
 log_filepath = os.path.join(log_dir,"running_logs.log")
 os.makedirs(log_dir, exist_ok=True)
 
-
 logging.basicConfig(
     level= logging.INFO,
     format= logging_str,
 
     handlers=[
-        logging.FileHandler(log_filepath),
-        logging.StreamHandler(sys.stdout)
+        logging.FileHandler(log_filepath),  # create log file
+        logging.StreamHandler(sys.stdout)   # print log in terminal
     ]
 )
 
